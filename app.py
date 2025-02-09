e4c4958e19df12589ea92b52c2aebd3c689ee1d7
Sam W. Courson <48110598+samcourson@users.noreply.github.com>
2025-02-09 15:36:33 -0500

    wtf

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 # app.py
 from flask import Flask, render_template
+from flask import send_from_directory
+import os
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="static")
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/static/<path:filename>')
+def static_files(filename):
+    return send_from_directory(os.path.join(app.root_path, 'static'), filename)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- freeze.py
+++ freeze.py
@@ -3,5 +3,9 @@ from app import app
 
 freezer = Freezer(app)
 
+@freezer.register_generator
+def freeze_static():
+    yield '/static/IMG_7331.jpg' 
+
 if __name__ == '__main__':
     freezer.freeze()
